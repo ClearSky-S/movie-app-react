f016ef0fc33e9d02abbd19c6e4b03bf387e933cf
ClearSky <ggogono2@gmail.com>
2022-01-11 17:42:19 +0900

    7.4

---
+++ src/routes/Detail.js
@@ -0,0 +1,13 @@
+
+import React, { useState, useEffect } from 'react';
+
+function Detail() {
+
+  return (
+    <div className="Detail">
+      Detail
+    </div>
+  );
+}
+
+export default Detail;
